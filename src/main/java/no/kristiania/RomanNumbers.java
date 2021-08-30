4869526e3b1c2fdd3fd23db56b65b1a713a7cf91
Jessica Fung <jessica-fcc@hotmail.com>
2021-08-30 19:13:44 +0200

    shouldConvert1ToI (fails)

---
+++ src/main/java/no/kristiania/RomanNumbers.java
@@ -0,0 +1,8 @@
+package no.kristiania;
+
+public class RomanNumbers {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/no/kristiania/RomanNumbersTest.java
@@ -0,0 +1,19 @@
+package no.kristiania;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumbersTest {
+
+    @Test
+    void shouldConvert1ToI() {
+        assertEquals("I", toRoman(1));      //Forventet og faktisk input
+    }
+
+    private String toRoman(int i) {
+        return null;
+    }
+
+
+}
